35d743d33e4e4d04a56f471850402740436c0756
Midnight Doggo <git@midnightdoggo19.com>
2026-07-03 08:23:02 +0100

    Initial commit

---
+++ src/main/java/com/midnightdoggo19/gatedaccess/Rcongate.java
@@ -0,0 +1,16 @@
+package com.midnightdoggo19.gatedaccess;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Rcongate implements ModInitializer {
+	public static final String MOD_ID = "gatedaccess";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Starting " + MOD_ID);
+	}
+}
---
+++ src/main/java/com/midnightdoggo19/gatedaccess/mixin/MinecraftDedicatedServerMixin.java
@@ -0,0 +1,27 @@
+package com.midnightdoggo19.gatedaccess.mixin;
+
+import com.midnightdoggo19.gatedaccess.Rcongate;
+import com.periut.cryonicconfig.CryonicConfig;
+import net.minecraft.server.dedicated.MinecraftDedicatedServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecraftDedicatedServer.class)
+public class MinecraftDedicatedServerMixin {
+	@Inject(at = @At("HEAD"), method = "executeRconCommand")
+	private void init(String command, CallbackInfoReturnable<String> cir) throws Exception {
+		String configCommands = CryonicConfig.getConfig(Rcongate.MOD_ID).getString("allowedCommands", "list");
+		String[] allowedCommands = configCommands.split(",");
+
+        for (String allowedCommand : allowedCommands) {
+            if (!command.startsWith(allowedCommand)) {
+				Rcongate.LOGGER.info("Blocked \"{}\" from running.", command);
+				throw new Exception("Command not allowed!");
+			}
+        }
+
+		Rcongate.LOGGER.info("RCON: " + command);
+	}
+}
